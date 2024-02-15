d200b7db64a48da141c0d85bdfa1bd25b699e7f9
abc <abc.@gmail.com>
2024-02-15 12:47:37 +0530

    Success

---
+++ SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    internal class SignIn
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                msg = "Username and Password required";
+            }
+            else
+            {
+                if ((username == "admin") && (password == "admin@123"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+            return msg; // Return the 'msg' variable
+        }
+    }
+}
+
